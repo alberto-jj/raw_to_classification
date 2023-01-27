81edfff3e862be98254ab8936273a7b354f69bc9
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2023-01-27 04:34:06 -0500

    harmonization and classification

--- 6_harmonization.py
+++ 6_harmonization.py
@@ -7,9 +7,10 @@ import itertools
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from neuroCombat import neuroCombat
+from neuroCombat import neuroCombat,neuroCombatFromTraining
 import numpy as np
 import copy
+from sklearn.model_selection import StratifiedKFold
 
 
 datasets = load_yaml('datasets.yml')
@@ -18,6 +19,10 @@ OUTPUT_DIR = PIPELINE['harmonization']['path']
 os.makedirs(OUTPUT_DIR,exist_ok=True)
 df_path = os.path.join(PIPELINE['aggregate']['path'],PIPELINE['aggregate']['filename'])
 df = pd.read_csv(df_path)
+
+MAX_FEATURES=PIPELINE['classification']['MAX_FEATURES']
+if MAX_FEATURES is not None:
+    df = df.iloc[:,:MAX_FEATURES]
 
 SP = PIPELINE['harmonization']['split']
 NC = PIPELINE['harmonization']['neuroCombat']
@@ -70,44 +75,83 @@ def combatModel(df,NC):
         categorical_cols=cat_no_batch)
     combat['data_cols']=data_cols
     combat['data_axes']=('features','samples')
+    combat['orig_data']=data
+    combat['batch_col']=batch_col
+    combat['batch_vals']=covars[batch_col]
+    combat['estimates']['batches']= combat['estimates']['batches'].astype(int)
     return combat
 
+df = dfFullNumber.copy()
 combat = combatModel(df,NC)
 np.save(os.path.join(OUTPUT_DIR,'combatFullData.npy'),combat)
 
-df = dfFullNumber.copy()
+
 X_train, X_test, y_train, y_test = train_test_split(df, df[SP['target']], test_size=SP['test'], random_state=42, stratify=df[SP['target']])
-idx_train= y_train.index
-idx_test = y_test.index
-combat_train = combatModel(X_train,NC)
-combat_test = combatModel(X_test,NC)
-combat_train['idx']=idx_train
-combat_test['idx']=idx_test
-np.save(os.path.join(OUTPUT_DIR,'combatTrainData.npy'),combat_train)
-np.save(os.path.join(OUTPUT_DIR,'combatTestData.npy'),combat_test)
 
-# Scaling
+def combatAndScale(X_train,X_test,y_train,y_test):
+    output = {}
+    #idx_train= y_train.index
+    #idx_test = y_test.index
+    combat_train = combatModel(X_train,NC)
+    combat_test = combatModel(X_test,NC)
+    combat_test = neuroCombatFromTraining(combat_test['orig_data'],combat_test['batch_vals'],combat_train['estimates'])
+    #combat_train['idx']=idx_train
+    #combat_test['idx']=idx_test
+    output['combat_train']=combat_train
+    output['combat_test']=combat_test
 
-# fit the scaler on the training data
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(combat_train['data'].transpose())
-features = combat_train['data_cols']
-# apply the same transformation to the test data
-X_test = scaler.transform(combat_test['data'].transpose())
-data_axes = ('samples','features')
-target_axes = ('label')
-y_train = y_train.to_numpy()
-y_test = y_test.to_numpy()
+    # Scaling
+    # fit the scaler on the training data
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(combat_train['data'].transpose())
+    features = combat_train['data_cols']
+    # apply the same transformation to the test data
+    X_test = scaler.transform(combat_test['data'].transpose())
+    data_axes = ('samples','features')
+    target_axes = ('label')
+    y_train = y_train.to_numpy()
+    y_test = y_test.to_numpy()
 
-data = {
-    'X_train':X_train,
-    'X_test':X_test,
-    'y_train':y_train,
-    'y_test':y_test,
-    'X_axes':data_axes,
-    'y_axes':target_axes,
-    'train_idx':idx_train,
-    'test_idx':idx_test
-}
+    output['data'] = {
+        'X_train':X_train,
+        'X_test':X_test,
+        'y_train':y_train,
+        'y_test':y_test,
+        'X_axes':data_axes,
+        'y_axes':target_axes,
+        #'train_idx':idx_train,
+        #'test_idx':idx_test,
+        'features':features
+    }
+    return output
 
-np.save(os.path.join(OUTPUT_DIR,'combatScaledData.npy'),data)
+singleSplit = combatAndScale(X_train, X_test, y_train, y_test)
+singleSplit['train_idx']=y_train.index
+singleSplit['test_idx']=y_test.index
+np.save(os.path.join(OUTPUT_DIR,'combatScaleSingleSplit.npy'),singleSplit)
+
+
+
+df = dfFullNumber.copy()
+cv    = StratifiedKFold(n_splits=SP['folds'], shuffle = True, random_state = 42)
+folds = [(train,test) for train, test in cv.split(df, df[SP['target']])]
+
+cv_dict = {}
+fold = 'fold-'
+for k,vals in enumerate(folds):
+    train,test = vals
+    cv_dict[fold+str(k)]={}
+    cv_dict[fold+str(k)]['train_idx']=train
+    cv_dict[fold+str(k)]['test_idx']=test
+    Y = df.copy()[SP['target']]
+    X_train = df.copy().iloc[train,:]
+    X_test = df.copy().iloc[test,:]
+    y_train = Y.iloc[train]
+    y_test = Y.iloc[test]
+    singleSplit = combatAndScale(X_train, X_test, y_train, y_test)
+    singleSplit['train_idx']=y_train.index
+    singleSplit['test_idx']=y_test.index
+
+    cv_dict[fold+str(k)]=singleSplit 
+
+np.save(os.path.join(OUTPUT_DIR,'combatScaleCV.npy'),cv_dict)
