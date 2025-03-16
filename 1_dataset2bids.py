1cc7ce3cf5e90925bcef4f7ffb3074b4e7139589
yjmantilla <yjmantilla@gmail.com>
2025-03-15 22:26:38 -0400

    feat: Refactor dataset2bids.py to accept pipeline file as argument and improve structure

--- 1_dataset2bids.py
+++ 1_dataset2bids.py
@@ -1,21 +1,29 @@
+import argparse
 from mne_bids import print_dir_tree # To show the input/output directories structures inside this example
 from sovabids.rules import apply_rules # Apply rules for conversion
 from sovabids.convert import convert_them # Do the conversion
 
 from eeg_raw_to_classification.utils import load_yaml
 
-cfg = load_yaml(f'pipeline.yml')
-datasets = load_yaml(cfg['datasets_file'])
+def main(pipeline_file):
+    cfg = load_yaml(pipeline_file)
+    datasets = load_yaml(cfg['datasets_file'])
 
-for dslabel,DATASET in datasets.items():
-    if DATASET.get('skip',False):
-        continue
+    for dslabel, DATASET in datasets.items():
+        if DATASET.get('skip', False):
+            continue
 
-    if 'sovabids' in DATASET:
-        source_path = DATASET['sovabids']['paths']['source_path']
-        bids_path = DATASET['sovabids']['paths']['bids_path']
-        rules = DATASET['sovabids']['rules']
+        if 'sovabids' in DATASET:
+            source_path = DATASET['sovabids']['paths']['source_path']
+            bids_path = DATASET['sovabids']['paths']['bids_path']
+            rules = DATASET['sovabids']['rules']
 
-        mappings = apply_rules(source_path,bids_path,rules)
-        convert_them(mappings)
-        print_dir_tree(bids_path)
+            mappings = apply_rules(source_path, bids_path, rules)
+            convert_them(mappings)
+            print_dir_tree(bids_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run the dataset to BIDS conversion pipeline.')
+    parser.add_argument('pipeline_file', type=str, help='Path to the pipeline YAML file')
+    args = parser.parse_args()
+    main(args.pipeline_file)
