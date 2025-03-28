9b778b4a65f59bbb652cd709351d21e1fb8c449b
yjmantilla <yjmantilla@gmail.com>
2025-03-28 00:39:16 -0400

    feat: Add SLURM job script for preprocessing pipeline execution

---
+++ jobs/preprocess.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+#SBATCH --time=0-00:10:00
+#SBATCH --job-name=example
+#SBATCH --output=%j-example.out
+#SBATCH --error=%j-example.err
+#SBATCH --mail-user=yjmantilla@gmail.com
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020
+module load StdEnv/2023
+module load python/3.11.5
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+##pip install --no-index --upgrade pip
+cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
+##pip install --no-index -r requirements.txt
+pip install -r requirements_minimal.txt
+pip install .
+python -u /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification/3_preprocess.py pipeline_saint.yml --external_jobs 1 --only_total
+
+
