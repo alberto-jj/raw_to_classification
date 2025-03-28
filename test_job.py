e4312612c8d86bd11f539eae17a57ab99cba8c59
yjmantilla <yjmantilla@gmail.com>
2025-03-28 00:42:07 -0400

    feat: Import feature extraction utilities in test_job.py

--- test_job.py
+++ test_job.py
@@ -1,3 +1,6 @@
 import mne
 
 print('MNE version:', mne.__version__)
+
+from eeg_raw_to_classification import features as feat
+from eeg_raw_to_classification.utils import load_yaml, save_dict_to_json
