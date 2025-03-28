53608647267d5ffc3026b28e7fd985aa8bf564e9
yjmantilla <yjmantilla@gmail.com>
2025-03-28 05:05:24 -0400

    feat: Add debug print statement to display available features in test_job.py

--- test_job.py
+++ test_job.py
@@ -4,3 +4,5 @@ print('MNE version:', mne.__version__)
 
 from eeg_raw_to_classification import features as feat
 from eeg_raw_to_classification.utils import load_yaml, save_dict_to_json
+
+print(dir(feat))
