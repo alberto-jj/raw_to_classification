7d8e1035dac3ca1e40ff15dca8b68788ca484602
yjmantilla <yjmantilla@gmail.com>
2025-03-28 01:35:59 -0400

    feat: Add SLURM job scripts for preprocessing and indexing, and update pipeline configuration

---
+++ jobs/array_preprocess.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=48G
+#SBATCH --time=0-02:00:00
+#SBATCH --array=0-1
+##0-1
+#SBATCH --job-name=preprocess
+#SBATCH --output=%A_%a-preprocess.out
+#SBATCH --error=%A_%a-preprocess.err
+#SBATCH --mail-user=yjmantilla@gmail.com
+#SBATCH --mail-type=ALL
+
+module load StdEnv/2020
+module load StdEnv/2023
+module load python/3.11.5
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+##pip install --no-index --upgrade pip
+cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
+##pip install --no-index -r requirements.txt
+pip install -r requirements_minimal.txt
+pip install .
+python -u 3_preprocess.py pipeline_saint.yml --index $SLURM_ARRAY_TASK_ID --external_jobs 1 --internal_jobs 32
+
+
--- jobs/get_indexes.sh
+++ jobs/get_indexes.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=4G
 #SBATCH --time=0-00:10:00
 #SBATCH --job-name=example
-#SBATCH --output=%j-example.out
-#SBATCH --error=%j-example.err
+#SBATCH --output=%j-indexes.out
+#SBATCH --error=%j-indexes.err
 #SBATCH --mail-user=yjmantilla@gmail.com
 #SBATCH --mail-type=ALL
 
@@ -20,6 +20,6 @@ cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
 ##pip install --no-index -r requirements.txt
 pip install -r requirements_minimal.txt
 pip install .
-python -u /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification/3_preprocess.py pipeline_saint.yml --external_jobs 1 --only_total
+python -u 4_preprocess.py pipeline_saint.yml --only_total
 
 
