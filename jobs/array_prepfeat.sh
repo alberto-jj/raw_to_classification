fa12529702b005a139c976fe3d2935db624bc06b
yjmantilla <yjmantilla@gmail.com>
2025-03-28 02:10:47 -0400

    feat: Add SLURM job script for preprocessing and feature extraction with array support

---
+++ jobs/array_prepfeat.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+#SBATCH --account=def-kjerbi
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=48G
+#SBATCH --time=0-02:00:00
+#SBATCH --array=0-2
+##0-250
+#SBATCH --job-name=preprocess
+#SBATCH --output=%A_%a-preprocess.out
+#SBATCH --error=%A_%a-preprocess.err
+## uncomment if you want to receive emails
+##SBATCH --mail-user=yjmantilla@gmail.com
+##SBATCH --mail-type=ALL
+
+module load StdEnv/2020
+module load StdEnv/2023
+module load python/3.11.5
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+##pip install --no-index --upgrade pip
+cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
+##pip install --no-index -r requirements.txt
+pip install -r requirements_minimal.txt
+pip install .
+python -u 3_preprocess.py pipeline_saint.yml --index $SLURM_ARRAY_TASK_ID --external_jobs 1 --internal_jobs 32
+python -u 4_features.py pipeline_saint.yml --index $SLURM_ARRAY_TASK_ID
+
+
