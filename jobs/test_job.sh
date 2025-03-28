02dd33892d4f984ca9b0560243fbeca8feef97bf
yjmantilla <yjmantilla@gmail.com>
2025-03-28 05:57:31 -0400

    feat: Update test_job.sh and requirements_extra.txt to include additional packages and improve environment setup

--- jobs/test_job.sh
+++ jobs/test_job.sh
@@ -14,7 +14,9 @@ module load StdEnv/2020
 module load StdEnv/2023
 module load python/3.11.5
 virtualenv --no-download $SLURM_TMPDIR/env
+##virtualenv --no-download env
 source $SLURM_TMPDIR/env/bin/activate
+##source env/bin/activate
 
 ##pip install --no-index --upgrade pip
 cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
