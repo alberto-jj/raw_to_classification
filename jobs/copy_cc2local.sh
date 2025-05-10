ccc1e30099df7cbf22571b0155b954c18c5c8b2b
yjmantilla <yjmantilla@gmail.com>
2025-05-10 14:41:46 -0400

    feat: Update rsync command in copy_cc2local script for improved data transfer

--- jobs/copy_cc2local.sh
+++ jobs/copy_cc2local.sh
@@ -1,2 +1,2 @@
 ## run in mobaxterm locally, not logged in to the cluster
-rsync -Lrh --progress yorguin@cedar.computecanada.ca:/home/yorguin/scratch/DeepCuriosity /media/Y/computecanada/DeepCuriosity
+rsync -r -h --copy-links --no-perms --progress yorguin@cedar.computecanada.ca:/home/yorguin/scratch/DeepCuriosity/ /media/Y/computecanada/swimmer
