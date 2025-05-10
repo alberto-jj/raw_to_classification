c26d6e829257448fb27204dd6cb0966bad42543b
yjmantilla <yjmantilla@gmail.com>
2025-05-10 14:31:10 -0400

    feat: Add script to copy DeepCuriosity data locally using rsync

---
+++ jobs/copy_cc2local.sh
@@ -0,0 +1,2 @@
+## run in mobaxterm locally, not logged in to the cluster
+rsync -Lrh --progress yorguin@cedar.computecanada.ca:/home/yorguin/scratch/DeepCuriosity /media/Y/computecanada/DeepCuriosity
