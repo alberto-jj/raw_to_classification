7cad81cfde888dbd9fdd637d091ead7139c2ccad
Yorguin Jose Mantilla Ramos <yorguin@cdr475.int.cedar.computecanada.ca>
2025-05-31 22:28:21 -0700

    improving compute canada job

--- jobs/array_prepfeat.sh
+++ jobs/array_prepfeat.sh
@@ -1,16 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 #SBATCH --account=def-kjerbi
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --time=0-02:00:00
 #SBATCH --array=0-977
-## 0-977, you need to get the index range from the get_indexes.sh script.
+## 0-977, you need to get the index range using the command below:
+## sbatch --export=STEP=index,PIPELINE_YML=project_files/pipeline_saint.yml --array=0 array_prepfeat.sh
 #SBATCH --job-name=prepfeat
 #SBATCH --output=%A_%a-prepfeat.out
 #SBATCH --error=%A_%a-prepfeat.err
 ## uncomment if you want to receive emails
 ##SBATCH --mail-user=yjmantilla@gmail.com
 ##SBATCH --mail-type=ALL
+
 
 ## Note, to test use:
 ## sbatch --export=STEP=3,PIPELINE_YML=project_files/pipeline_saint.yml --array=0-2 array_prepfeat.sh
@@ -20,11 +22,13 @@
 ## sbatch --export=STEP=3,PIPELINE_YML=project_files/pipeline_saint.yml array_prepfeat.sh
 ## sbatch --export=STEP=4,PIPELINE_YML=project_files/pipeline_saint.yml array_prepfeat.sh
 
+## To get the total number of indexes for array job configuration:
+## sbatch --export=STEP=index,PIPELINE_YML=project_files/pipeline_saint.yml --array=0 array_prepfeat.sh
+
 ## You can use this job as an example of how to run in on an interactive session.
 ## Remind that you need to have installed the virtual environment before running this script.
 
-##virtualenv --no-download $SLURM_TMPDIR/env
-##virtualenv --no-download raw_to_classification_env
+
 
 ENV=${ENV:-DEFAULT}
 if [ "$ENV" == "TMP" ]; then
@@ -43,6 +47,9 @@ elif [ "$ENV" == "DEFAULT" ]; then
     ## module load StdEnv/2020
     module load StdEnv/2023
     module load python/3.11.5
+
+    ## The commented lines below should have already been done in the environment setup.
+    ## virtualenv --no-download /home/yorguin/envs/raw_to_classification_env
     source /home/yorguin/envs/raw_to_classification_env/bin/activate
     cd /home/yorguin/projects/def-kjerbi/yorguin/raw_to_classification
     ## pip install --no-index -r requirements_cc_noindex.txt
@@ -52,6 +59,10 @@ fi
 
 # Set default YAML path if not passed
 PIPELINE_YML=${PIPELINE_YML:-project_files/pipeline_saint.yml}
+
+if [ ! -f "$PIPELINE_YML" ]; then
+    echo "Warning: YAML file '$PIPELINE_YML' does not exist!"
+fi
 
 # Decide which step to run based on $STEP
 if [ "$STEP" == "3" ]; then
