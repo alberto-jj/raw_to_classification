9cc4064757cd25672fcaeb2cb1b29351b1fb23a1
Yorguin Jose Mantilla Ramos <yorguin@cedar1.int.cedar.computecanada.ca>
2025-06-06 13:33:11 -0700

    minor updates

--- jobs/copy_cc2local.sh
+++ jobs/copy_cc2local.sh
@@ -3,3 +3,7 @@ rsync -r -h --copy-links --no-perms --progress yorguin@cedar.computecanada.ca:/h
 
 
 rsync -r -h --copy-links --no-perms --progress yorguin@narval.computecanada.ca:/home/yorguin/scratch/saint/aggregate/FeaturesChannels@prep-defaultprep /media/Y/computecanada/swimmer
+
+
+ssh yorguin@cedar.computecanada.ca     "find /home/yorguin/scratch/data/MEG_*/derivatives/features@prepDur30Ov20/ -type f -name '*.npy'" > files.txt
+rsync -h --copy-links --no-perms --progress --files-from=files.txt     --relative yorguin@cedar.computecanada.ca:/ /media/Y/computecanada/cocosprint
