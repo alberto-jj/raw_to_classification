60c65eaf03a9a24e526341bc76b1f22c0bfc5100
yjmantilla <yjmantilla@gmail.com>
2025-06-08 08:13:11 -0400

    feat: refactor bidsify logic to support dynamic module import and streamline dataset processing

--- scripts/1_dataset2bids.py
+++ scripts/1_dataset2bids.py
@@ -2,6 +2,7 @@ import argparse
 from mne_bids import print_dir_tree # To show the input/output directories structures inside this example
 from sovabids.rules import apply_rules # Apply rules for conversion
 from sovabids.convert import convert_them # Do the conversion
+import importlib
 
 from eeg_raw_to_classification.utils import load_yaml,get_path
 
@@ -9,21 +10,34 @@ def main(pipeline_file):
     cfg = load_yaml(pipeline_file)
     MOUNT = cfg.get('mount', None)
     datasets = load_yaml(get_path(cfg['datasets_file'],MOUNT))
+    this_dataset2bids = cfg.get('1_dataset2bids', {})
+
+    if 'redefine_bidsify' in this_dataset2bids:
+        import_dict = this_dataset2bids['redefine_bidsify']
+        module_name = import_dict['from_this']
+        func_name = import_dict['import_that']
+        module = importlib.import_module(module_name)
+        bidsify = getattr(module, func_name)
+    else:
+        def bidsify(source_path, bids_path, DATASET_CFG):
+            rules = DATASET_CFG['bidsify']['rules'] # this is suppose to be the dictionary of rules
+            mappings = apply_rules(source_path, bids_path, rules)
+            convert_them(mappings)
+            print_dir_tree(bids_path)
 
     for dslabel, DATASET in datasets.items():
         if DATASET.get('skip', False):
             continue
 
-        if 'sovabids' in DATASET:
-            source_path = DATASET['sovabids']['paths']['source_path']
-            source_path = get_path(source_path, MOUNT)
-            bids_path = DATASET['sovabids']['paths']['bids_path']
-            bids_path = get_path(bids_path, MOUNT)
-            rules = DATASET['sovabids']['rules'] # this is suppose to be the dictionary of rules
+        bidsify_cfg = DATASET.get('bidsify', None)
 
-            mappings = apply_rules(source_path, bids_path, rules)
-            convert_them(mappings)
-            print_dir_tree(bids_path)
+        if bidsify_cfg :
+            source_path = DATASET['bidsify']['paths']['source_path']
+            source_path = get_path(source_path, MOUNT)
+            bids_path = DATASET['bidsify']['paths']['bids_path']
+            bids_path = get_path(bids_path, MOUNT)
+
+            bidsify(source_path, bids_path, DATASET)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the dataset to BIDS conversion pipeline.')
