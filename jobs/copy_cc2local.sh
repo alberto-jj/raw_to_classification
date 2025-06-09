7b77a3638fe6a3d9f4c101febcb4749304ef1e1b
yjmantilla <yjmantilla@gmail.com>
2025-06-09 00:55:20 -0400

    feat: enhance data synchronization script with additional file handling and directory setup

--- jobs/copy_cc2local.sh
+++ jobs/copy_cc2local.sh
@@ -21,3 +21,24 @@ ssh yorguin@cedar.computecanada.ca     "find /home/yorguin/scratch/data/MEG_*/de
 ssh yorguin@cedar.computecanada.ca "find /home/yorguin/scratch/data/MEG_*/meg_data_BIDS/" > files.txt
 rsync -h --copy-links --no-perms --progress --dry-run --files-from=files.txt --relative yorguin@cedar.computecanada.ca:/ /home/yorguin/scratch/data/
 rsync -h --copy-links --no-perms --progress --dry-run --files-from=files.txt --relative yorguin@cedar.computecanada.ca:/ ~/scratch/data/
+
+
+find /home/yorguin/scratch/data/MEG_*/meg_data_BIDS/ > bids_files.txt
+
+## To see the total size of all files listed 
+xargs -a bids_files.txt du -ch | tail -1
+
+sed 's|/home/yorguin/scratch/data/||' bids_files.txt > rel_bids_files.txt
+
+## https://docs.alliancecan.ca/wiki/Project_layout
+
+mkdir $HOME/projects/def-kjerbi/data_sprint
+setfacl -d -m g::rwx $HOME/projects/def-kjerbi/data_sprint
+chmod g+s $HOME/projects/def-kjerbi/data_sprint
+
+## this works, not sure why rsync ends up with the files assigned to the personal group (almost no space there)
+cat rel_bids_files.txt | xargs -I{} cp --parents "{}" /home/yorguin/projects/def-kjerbi/data_sprint
+
+## This does not work, disk quota exceeded
+##cd /home/yorguin/scratch/data
+##rsync -av --files-from=rel_bids_files.txt /home/yorguin/scratch/data/ /project/def-kjerbi/data_sprint/
