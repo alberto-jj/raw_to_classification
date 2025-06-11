d571cb4f2f872586d89936f3c047a873573fdc50
Yorguin Jose Mantilla Ramos <yorguin@cedar2.int.cedar.computecanada.ca>
2025-06-10 21:37:55 -0700

    testing phi

--- jobs/do.sh
+++ jobs/do.sh
@@ -41,3 +41,9 @@ python -u "./scripts/4_features.py" "project_files/pipeline_cocosprint.yml" --in
 python -u "./scripts/4_features.py" "project_files/pipeline_cocosprint.yml" --retry_errors --raise_on_error
 
 python -u scripts/5_aggregate.py project_files/pipeline_cocosprint.yml > 5_aggregate_cocosprint.log
+
+
+## this are for phi features
+python -u "./scripts/4_features.py" "project_files/pipeline_cocosprint2.yml" --inspect_only
+
+python -u "./scripts/4_features.py" "project_files/pipeline_cocosprint2.yml" --index 67 --retry_errors
