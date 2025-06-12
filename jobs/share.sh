de415de48d92ee9c0e496f2017ac6b325de4496d
Yorguin Jose Mantilla Ramos <yorguin@cdr477.int.cedar.computecanada.ca>
2025-06-12 01:18:29 -0700

    minor

--- jobs/share.sh
+++ jobs/share.sh
@@ -1,10 +1,10 @@
 # List of folders
 folders=(
-    /home/yorguin/scratch/data/MEG_ketamine
-    # /home/yorguin/scratch/data/MEG_LSDV2
-    # /home/yorguin/scratch/data/MEG_perampanel
-    # /home/yorguin/scratch/data/MEG_psilocybin
-    # /home/yorguin/scratch/data/MEG_tiagabine
+    #/home/yorguin/scratch/data/MEG_ketamine
+    #/home/yorguin/scratch/data/MEG_LSDV2
+    /home/yorguin/scratch/data/MEG_perampanel
+    /home/yorguin/scratch/data/MEG_psilocybin
+    /home/yorguin/scratch/data/MEG_tiagabine
 )
 
 # Loop through each folder
@@ -17,15 +17,15 @@ for folder in "${folders[@]}"; do
 
     # Set correct permissions on folders
     echo "Setting permissions for directories in $folder"
-    find "$folder" -type d -exec chmod g+rx {} \;
+    find "$folder" -type d -print -exec chmod g+rx {} \;
 
     # Set correct permissions on files
     echo "Setting permissions for files in $folder"
-    find "$folder" -type f -exec chmod g+r {} \;
+    find "$folder" -type f -print -exec chmod g+r {} \;
 
     # Ensure new files inherit group
     echo "Setting setgid bit for directories in $folder"
-    find "$folder" -type d -exec chmod g+s {} \;
+    find "$folder" -type d -print -exec chmod g+s {} \;
 
     echo "Done with $folder"
     echo "-----------------------------"
